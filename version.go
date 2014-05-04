3bfa78dc56877720bc64fe11624db62085a2fa18
Ian Kent <email@iankent.co.uk>
2014-05-04 10:37:22 +0100

    Add version file

---
+++ version.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+const (
+	AppName         = "go-log"
+	AppVersionMajor = 0
+	AppVersionMinor = 1
+)
+
+// revision part of the program version.
+// This will be set automatically at build time like so:
+//
+//     go build -ldflags "-X main.AppVersionRev `date -u +%s`"
+var AppVersionRev string
+
+func Version() string {
+	if len(AppVersionRev) == 0 {
+		AppVersionRev = "0"
+	}
+
+	return fmt.Sprintf("%s %d.%d.%s (Go runtime %s).\nCopyright (c) 2014, Ian Kent.",
+		AppName, AppVersionMajor, AppVersionMinor, AppVersionRev, runtime.Version())
+}
